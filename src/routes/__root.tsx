65953cab0c2abc3260dc905112714de260d1c6b3
Ivan <ivanfilho746@gmail.com>
2025-08-05 12:49:13 -0300

    teste v3

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ export const Route = createRootRoute({
     <>
       <div className="p-2 flex gap-2">
         <Link to="/">Home</Link>
+        <p>Sla</p>
       </div>
       <hr />
       <Outlet />
