516de13fed5eaa6f6fe7d17a850aefe99b2e56c5
Ivan <ivanfilho746@gmail.com>
2025-08-19 07:09:09 -0300

    merge teste

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,7 @@ export const Route = createRootRoute({
       <div className="p-2 flex gap-2">
         <Link to="/">Home</Link>
         <p>Sla</p>
+        <p>AAAAAAAAAAAAAAAAAAAAAA</p>
       </div>
       <hr />
       <Outlet />
