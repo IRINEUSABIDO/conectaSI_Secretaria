b375c89fe4faf89fe66edacec65267d835c805fe
seila1828e3 <discorddelvictin@gmail.com>
2025-10-28 14:41:43 -0300

    feat: tela de login e de criar contas feitas

--- src/components/buttonPassword.tsx
+++ src/components/buttonPassword.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import hide_gray from "../assets/icons/Hide_gray.png";
-import show_gray from "../assets/icons/Show_gray.png";
+import hide from "../assets/IconsNewSvgs/passwordHide.svg";
+import show from "../assets/IconsNewSvgs/passwordShow.svg";
 interface Props {
   title: string;
   placeholder?: string;
@@ -25,25 +25,24 @@ export const Button_password = ({
         <h1 className="font-xs font-bold text-header-blue ml-3">{title}</h1>
 
         {hasPassword ? (
-          <div className="rounded-3xl bg-white border-2 border-header-light-blue flex items-center space-x-6">
+          <div className="rounded-3xl bg-white border-2 border-header-light-blue flex items-center justify-between w-[90vw] sm:w-[600px]">
             <input
               placeholder={placeholder}
-              //troca os tipo de string
               type={showPassword ? "text" : "password"}
-              className="px-20 py-4 text-header-blue font-inter font-semibold outline-0"
+              className="pl-4 pr-10 py-4 text-header-blue font-inter font-semibold outline-0"
             />
             <button type="button" onClick={click} className="outline-none">
               {" "}
               <img
                 //troca as imagens de acordo com o tipo string definido
-                src={showPassword ? show_gray : hide_gray}
+                src={showPassword ? show : hide}
                 alt="Altenar imagens quando quiser mostrar ou nao sua senha"
                 className="w-[25px] m-[2px] cursor-pointer"
               />
             </button>
           </div>
         ) : (
-          <div className="rounded-2xl bg-light-gray flex items-center space-x-6">
+          <div className="rounded-2xl bg-light-gray flex items-center">
             <input
               placeholder={placeholder}
               type="password"
--- src/routes/criarConta.tsx
+++ src/routes/criarConta.tsx
@@ -2,51 +2,61 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Button_Input } from "../components/buttonInput";
 import { Button_cpf } from "../components/buttonCPF";
 import ConectaSeinfraIcon from "../assets/IconsNewSvgs/ConectaSeinfra.svg";
-import LogoPrefeitura from "../assets/logoPrefeitura.png";
+import LogoPrefeitura from "../assets/IconsNewSvgs/LogoPrefeitura.svg";
+import pinkLine from "../assets/IconsNewSvgs/pinkLine.svg";
+import yellowLine from "../assets/IconsNewSvgs/yellowLine.svg";
 
 export const Route = createFileRoute("/criarConta")({
-    component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-    return (
-        <>
-            {/* HEADER */}
-            <div className="flex flex-col overflow-x-hidden">
-                <header>
-                    <div className="flex items-center justify-center">
-                        <h1 className="font-semibold mt-[5%] text-4xl text-header-blue">
-                            Criar Conta
-                        </h1>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <h1 className="font-light text-xl mt-[1%] text-header-blue">
-                            Preencha as informações obrigatórias para criar sua conta
-                        </h1>
-                    </div>
-                </header>
+  return (
+    <>
+      <img src={pinkLine} alt="Linha Rosa Background" />
+      {/* HEADER */}
+      <div className="flex flex-col overflow-x-hidden space-y-18">
+        <header>
+          <div className="flex items-center justify-center">
+            <h1 className="font-semibold mt-[5%] text-5xl text-header-blue">
+              Criar Conta
+            </h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <h1 className="font-light text-xl mt-[4%] text-header-blue">
+              Preencha as informações obrigatórias para criar sua conta
+            </h1>
+          </div>
+        </header>
 
-                {/* MAIN */}
-                <main className="mt-[2%]">
-                    <div className="flex flex-col justify-center items-center ">
+        {/* MAIN */}
+        <main className="mt-[2%]">
+          <div className="flex flex-col justify-center items-center ">
+            {/* BOTÕES: COMPONENTES */}
+            <Button_Input type="text" title="Nome" />
+            <Button_Input type="tel" title="Telefone" />
+            <Button_cpf title="CPF" />
 
-                        {/* BOTÕES: COMPONENTES */}
-                        <Button_Input type="text" title="Nome" />
-                        <Button_Input type="tel" title="Telefone" />
-                        <Button_cpf title="CPF" />
+            <button className="bg-header-blue w-[80vw] sm:w-auto sm:px-46 py-4 rounded-3xl text-white font-bold cursor-pointer">
+              <h1>Continuar</h1>
+            </button>
+          </div>
+        </main>
 
-                        <button className="bg-header-blue  w-[80vw] sm:w-auto sm:px-46 py-4 rounded-3xl text-white font-bold cursor-pointer">
-                            <h1>Continuar</h1>
-                        </button>
-                    </div>
-                </main>
+        {/* FOOTER */}
+        <footer className="flex items-cente justify-center flex-col">
+          <div className="flex items-center justify-center mt-[5%] mb-[5%] space-x-32">
+            <img src={ConectaSeinfraIcon} alt="Logo do Conecta Seinfra" />
+            <img src={LogoPrefeitura} alt="LogoPrefeitura de Nova Russas" />
+          </div>
 
-                {/* FOOTER */}
-                <footer className="flex items-center justify-center mt-[5%] mb-[5%] space-x-32">
-                    <img src={ConectaSeinfraIcon} alt="Logo do Conecta Seinfra" />
-                    <img src={LogoPrefeitura} alt="LogoPrefeitura de Nova Russas" />
-                </footer>
-            </div>
-        </>
-    );
+          <img
+            src={yellowLine}
+            alt="Yellow Line"
+            className="absolute right-0 "
+          />
+        </footer>
+      </div>
+    </>
+  );
 }
