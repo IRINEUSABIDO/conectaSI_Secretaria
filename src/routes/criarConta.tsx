1f9056bba587cae82356ddca0d46c3759038f3a2
seila1828e3 <discorddelvictin@gmail.com>
2025-10-28 10:41:09 -0300

    teste

--- src/routes/criarConta.tsx
+++ src/routes/criarConta.tsx
@@ -43,7 +43,7 @@ function RouteComponent() {
         {/* FOOTER */}
         <footer className="flex items-center justify-center mt-[5%] mb-[5%] space-x-8">
         <img src={ConectaSeinfraIcon} alt="Logo do Conecta Seinfra"/>
-        <img src={LogoPrefeitura} alt="Logo da Prefeitura de Nova Russas" />
+        <img src={LogoPrefeitura} alt="LogoPrefeitura de Nova Russas" />
         </footer>
       </div>
     </>
